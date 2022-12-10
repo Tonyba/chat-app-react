3a4fe707c5aa3b0d2dc72c3ec87f4806dd1cb79e
Tonyba <37878200+Tonyba@users.noreply.github.com>
2022-12-10 19:08:14 -0400

    create conversation & added it to redux store

--- src/components/forms/CreateConversationForm.tsx
+++ src/components/forms/CreateConversationForm.tsx
@@ -1,32 +1,52 @@
-import React from 'react'
 import { InputField, InputContainer, InputLabel, TextField, Button } from '../../utils/styles/index';
 
 import styles from './index.module.scss';
 import { useDispatch } from 'react-redux';
-import { addConversation } from '../../store/conversationSlide';
+import { addConversation, createConversationThunk } from '../../store/conversationSlide';
+import { useForm } from 'react-hook-form';
+import { CreateConversationParams } from '../../utils/types';
+import { AppDispatch } from '../../store';
+import { Dispatch, FC }  from 'react'
+import React from 'react';
 
-export const CreateConversationForm = () => {
+type Props = {
+    setShowModal: React.Dispatch<React.SetStateAction<boolean>>
+}
 
-  const dispatch = useDispatch();
+export const CreateConversationForm: FC<Props> = ({ setShowModal }) => {
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const { register, handleSubmit, formState: { errors } } = useForm<CreateConversationParams>({});
+
+  const onSubmit = (data: CreateConversationParams) => {
+        console.log(data);
+        dispatch(createConversationThunk(data))
+        .then((data) => {
+            console.log(data);
+            setShowModal(false);
+            
+        })
+        .catch((err) => console.log(err));
+  }
+  
 
   return (
-    <form className={styles.createConversationForm} >
+    <form className={styles.createConversationForm} onSubmit={handleSubmit(onSubmit)} >
         <section>
             <InputContainer backgroundColor='#161616' >
                 <InputLabel>Recipient</InputLabel>
-                <InputField />
+                <InputField  {...register('email', { required: 'Email is required' })} />
             </InputContainer>
         </section>
         <section className={styles.message} >
             <InputContainer backgroundColor='#161616' >
                 <InputLabel>Message (optional)</InputLabel>
-                <TextField />
+                <TextField {...register('message', { required: 'Message is required' })} />
             </InputContainer>
         </section>    
 
-        <Button onClick={(e) =>{ 
-            e.preventDefault();        
-        }} >Create Conversation</Button>
+        <Button type='submit'>Create Conversation</Button>
         
     </form>
   )
