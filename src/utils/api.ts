dc04630b8c1672d66891556b473193991d3ac738
Tony <anthonymessagesab@gmail.com>
2023-02-16 20:40:26 -0400

    messages api fixed

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,19 +1,41 @@
-import axios, { AxiosRequestConfig } from 'axios';
-import { createUserParams, loginUserParams, User, Conversation, CreateMessageParams, FetchMessagePayload, CreateConversationParams } from './types';
+import axios, { AxiosRequestConfig } from "axios";
+import {
+  createUserParams,
+  loginUserParams,
+  User,
+  Conversation,
+  CreateMessageParams,
+  FetchMessagePayload,
+  CreateConversationParams,
+} from "./types";
 
 const API_URL = process.env.REACT_APP_API_URL;
 const config: AxiosRequestConfig = { withCredentials: true };
 
-export const postRegisterUser = (data: createUserParams) => axios.post(`${API_URL}/auth/register`, data, config);
+export const postRegisterUser = (data: createUserParams) =>
+  axios.post(`${API_URL}/auth/register`, data, config);
 
-export const postLoginUser = (data: loginUserParams) => axios.post(`${API_URL}/auth/login`, data, config);
+export const postLoginUser = (data: loginUserParams) =>
+  axios.post(`${API_URL}/auth/login`, data, config);
 
-export const getAuthUser = () => axios.get<User>(`${API_URL}/auth/status`, config);
+export const getAuthUser = () =>
+  axios.get<User>(`${API_URL}/auth/status`, config);
 
-export const getConversations = () => axios.get<Conversation[]>(`${API_URL}/conversations`, config);
+export const getConversations = () =>
+  axios.get<Conversation[]>(`${API_URL}/conversations`, config);
 
-export const getConversationMessages = (id: number) => axios.get<FetchMessagePayload>(`${API_URL}/messages/${id}`, config);
+export const getConversationMessages = (id: number) =>
+  axios.get<FetchMessagePayload>(
+    `${API_URL}/conversations/${id}/messages`,
+    config
+  );
 
-export const postNewMessage = (data: CreateMessageParams) => axios.post(`${API_URL}/messages`, data, config);
+export const postNewMessage = (data: CreateMessageParams) =>
+  axios.post(
+    `${API_URL}/conversations/${data.conversationId}/messages`,
+    data,
+    config
+  );
 
-export const postNewConversation = (data: CreateConversationParams) => axios.post<Conversation>(`${API_URL}/conversations`, data, config);
+export const postNewConversation = (data: CreateConversationParams) =>
+  axios.post<Conversation>(`${API_URL}/conversations`, data, config);
